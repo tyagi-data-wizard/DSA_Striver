1bf93512a1687e6a8db39d2ae336e4b3083a6936
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-24 23:17:50 +0530

    pattern10

---
+++ src/Pattern/Pattern10.java
@@ -0,0 +1,31 @@
+package Pattern;
+
+public class Pattern10 {
+
+    static void printTriangle(int n) {
+        //first n
+        for(int row=0;row<n;row++){
+            //stars
+            for(int col=0;col<row+1;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+        //last n-1
+        for(int row=0;row<n-1;row++){
+
+            for(int col=0;col<n-1-row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(6);
+    }
+}
