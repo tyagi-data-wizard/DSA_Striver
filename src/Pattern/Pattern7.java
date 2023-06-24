65605982a60986c8aa89753ee9467a1e563f83ce
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-24 20:41:18 +0530

    pattern7 & 8

---
+++ src/Pattern/Pattern7.java
@@ -0,0 +1,33 @@
+package Pattern;
+
+public class Pattern7 {
+
+    static void printTriangle(int n) {
+
+        for (int row = 0; row < n; row++) {
+            //spaces
+            for (int col = 0; col < n - row - 1; col++) {
+                System.out.print(" ");
+            }
+
+            //star
+            for (int col = 0; col < 2 * row + 1; col++) {
+                System.out.print("*");
+
+            }
+
+            //remaining space
+            for (int col = 0; col < n - row - 1; col++) {
+                System.out.print(" ");
+
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(6);
+    }
+}
---
+++ src/Pattern/Pattern8.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+public class Pattern8 {
+
+    static void printTriangle(int n) {
+        for(int row=0;row<n;row++){
+            //0 fspace 9 star   0 bspace
+            //1 fspace 7star    1 bspace
+
+            for(int col=0;col<row;col++){
+                System.out.print(" ");
+            }
+
+            for(int col=0;col<2*(n-row)-1;col++){
+                System.out.print("*");
+            }
+
+
+
+            for(int col=0;col<row;col++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(6);
+    }
+}
