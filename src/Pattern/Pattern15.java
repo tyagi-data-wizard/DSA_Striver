f1a754361fd5a7309ff5afc72b99385f4f68fc73
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-25 17:04:10 +0530

    pattern15

---
+++ src/Pattern/Pattern15.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class Pattern15 {
+
+    static void printTriangle(int n) {
+
+
+        for(int row=0;row<n;row++){
+            char alpha = 'A';
+            for(int col=0;col<n-row;col++){
+                System.out.print(alpha);
+                alpha++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(6);
+    }
+}
