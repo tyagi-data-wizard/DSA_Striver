c3a988b246c246a75f9232dc502278718e12d4e6
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-25 16:54:36 +0530

    pattern13

---
+++ src/Pattern/Pattern13.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class Pattern13 {
+
+    static void printTriangle(int n) {
+
+        int count=1;
+
+        for(int row=0;row<n;row++){
+            for(int col=0;col<=row;col++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(6);
+    }
+}
