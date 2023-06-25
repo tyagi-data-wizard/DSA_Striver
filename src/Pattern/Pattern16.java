edc6ddda3c291ea84fd6746bc7f829fd9d0aa0ba
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-25 17:08:30 +0530

    pattern16

---
+++ src/Pattern/Pattern16.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class Pattern16 {
+
+    static void printTriangle(int n) {
+
+        char alpha = 'A';
+        for(int row=0;row<n;row++){
+            for(int col=0;col<=row;col++){
+                System.out.print(alpha);
+            }
+            System.out.println();
+            alpha++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printTriangle(6);
+    }
+}
