311dd7cf92b61d751efbc17c93268abfe56e8bbf
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-27 18:03:10 +0530

    PrimeNumber naive

---
+++ src/BasicMaths/PrimeNumber.java
@@ -0,0 +1,22 @@
+package BasicMaths;
+
+public class PrimeNumber {
+
+    static boolean checkPrimeNaive(int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        if(checkPrimeNaive(11)){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Non-Prime");
+        }
+    }
+}
