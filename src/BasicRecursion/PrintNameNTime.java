5f15a9b44162a438dc94f88bc1afa2912b88788f
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-27 23:08:42 +0530

    chnge while to if

--- src/BasicRecursion/PrintNameNTime.java
+++ src/BasicRecursion/PrintNameNTime.java
@@ -3,12 +3,11 @@ package BasicRecursion;
 public class PrintNameNTime {
 
     static void printNameNTime(int n, String name){
-        while(n>0){
-            printNameNTime(n-1,name);
-            System.out.println(name);
+        if(n==0){
             return;
         }
-
+        printNameNTime(n-1,name);
+        System.out.println(name);
     }
 
     public static void main(String[] args) {
