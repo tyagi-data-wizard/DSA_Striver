148907a997bf8adc9e15084c98685c15a33796cc
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-28 17:11:16 +0530

    SumOfFirstN using Recursion

---
+++ src/BasicRecursion/SumofNNumbers.java
@@ -0,0 +1,28 @@
+package BasicRecursion;
+
+public class SumofNNumbers {
+
+    static int sumRecursionFunctional(int n){
+        if(n==0){
+            return 0;
+        }
+        return n + sumRecursionFunctional(n-1);
+    }
+
+    static void sumRecursionParamterized(int n, int sum){
+        if(n<1){
+
+            System.out.println(sum);
+            return;
+        }
+
+        // Function call to increment sum by i till i decrements to 1.
+        sumRecursionParamterized(n-1,sum+i);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumRecursionFunctional(5));
+        sumRecursionParamterized(5,0);
+
+    }
+}
