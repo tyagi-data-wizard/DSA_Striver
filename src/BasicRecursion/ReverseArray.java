e565ffb221c03f8b9ae33e147d910f752123231a
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-06-29 11:33:34 +0530

    actually reversing array

--- src/BasicRecursion/ReverseArray.java
+++ src/BasicRecursion/ReverseArray.java
@@ -11,10 +11,29 @@ public class ReverseArray {
 
     }
 
+
+    static void reverseArraySec(int[] arr, int start, int end){
+            if(start<end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                reverseArraySec(arr, start + 1, end - 1);
+            }
+    }
+
     public static void main(String[] args) {
         int[] arr =new int[]{1,2,3,3,4};
         System.out.println(arr.toString());
 
         reverseArray(arr, 0);
+
+        System.out.println();
+
+        reverseArraySec(arr, 0, arr.length-1);
+
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
     }
 }
