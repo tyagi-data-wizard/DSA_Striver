01e0bc549b54972b2784806499894daf0de162bf
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-07-01 23:01:38 +0530

    freq of element using hashing

---
+++ src/BasicHashing/FrequencyOfElementInArray.java
@@ -0,0 +1,37 @@
+package BasicHashing;
+
+import java.util.HashMap;
+
+public class FrequencyOfElementInArray {
+
+    static void frequencyCountNaive(int[] arr){
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i : arr){
+
+            if(map.containsKey(i)){
+                map.put(i, map.get(i)+1);
+            }else {
+                map.put(i, 1);
+            }
+        }
+
+//        for(int i : arr ){
+//
+//            System.out.println("key "+ i +" is "+ map.get(i) +" times.");
+//        }
+        for (Integer name: map.keySet())
+        {
+            System.out.println(name + " : " +map.get(name));
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,2,4,4,6,6};
+
+        frequencyCountNaive(arr);
+
+    }
+}
