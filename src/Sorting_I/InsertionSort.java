47be97a545f7900ad94d3eca939ac6e7362df41b
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-07-04 13:26:23 +0530

    insertion sort

--- src/Sorting_I/InsertionSort.java
+++ src/Sorting_I/InsertionSort.java
@@ -1,12 +1,51 @@
 package Sorting_I;
 
+import java.util.Scanner;
+
 public class InsertionSort {
 
-    private static void insertionSort(){
-        g
+    public static void main(String[] args) {
+        int[] arr = inputArray();
+
+        System.out.println("UnSorted Array : ");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("\nSorted Array : ");
+        printArray(arr);
     }
 
-    public static void main(String[] args) {
+    private static void insertionSort(int[] arr){
 
+        for (int i = 1; i < arr.length; i++) {
+
+            for (int j = i; j > 0; j--) {
+                if(arr[j]<arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+
+            }
+        }
+
+    }
+    private static void printArray(int[] arr){
+        for (int i:arr) {
+            System.out.print(i+ " ");
+        }
+    }
+    private static int[] inputArray() {
+        System.out.println("Enter length of array");
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] arr = new int[len];
+        System.out.println("Enter array elements");
+        for (int i = 0; i <len; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
     }
 }
