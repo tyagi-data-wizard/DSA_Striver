0a7b0591ff309790a7eccb62dc4c70413d4727ff
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-09-02 11:45:37 +0530

    move zero to end optimal

--- src/EasyArrayProblems/ShiftZeros.java
+++ src/EasyArrayProblems/ShiftZeros.java
@@ -7,6 +7,30 @@ public class ShiftZeros {
 
     //Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
     //Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
+
+    private static void moveZeroToEndOptimal(int[] arr){
+        int j = -1;             //index for zero
+        //find the first zero
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        //start iterating and swapping
+        for(int i=j+1;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i]= arr[j];
+                arr[j]= temp;
+                j++;
+            }
+        }
+
+
+    }
+
     private static int[] moveZeroToEnd(int[] arr){
         int zeroCounter=0;
         int counter=0;
@@ -23,6 +47,16 @@ public class ShiftZeros {
 
         for(int i =counter;i< temp.length;i++){
             temp[i] = 0;
+        }
+
+        //changing the input array
+        for(int i=0;i<counter;i++){
+            arr[i] = temp[i];
+        }
+
+        //now setting rest of the elements as zero
+        for(int i=counter;i<arr.length;i++){
+            arr[i]=0;
         }
         return temp;
     }
@@ -48,9 +82,14 @@ public class ShiftZeros {
     public static void main(String[] args) {
         int[] arr = inputArray();
 
-        int[] finalArr =  moveZeroToEnd(arr);
+        //int[] finalArr =  moveZeroToEnd(arr);
 
-        printArray(finalArr);
+        //printArray(finalArr);
+
+        //System.out.println("\ninput array changed: ");printArray(arr);
+
+        moveZeroToEndOptimal(arr);
+        printArray(arr);
 
 
     }
