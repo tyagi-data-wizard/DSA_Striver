f989074fd1bf3834a6f7f78335c6df865a77cc93
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-12-15 17:36:27 +0530

    missing number by XOR

--- src/EasyArrayProblems/MissingNumberInArray.java
+++ src/EasyArrayProblems/MissingNumberInArray.java
@@ -26,6 +26,33 @@ public class MissingNumberInArray {
         return sum;
     }
 
+    public static int findMissingNumHash(int[] arr, int N){
+        int[] hashArray = new int[N+1];
+        for (int i=0;i<arr.length;i++){
+                hashArray[arr[i]]++;
+        }
+
+        for (int i = 1; i < N; i++) {
+            if (hashArray[i]==0){
+                return i;
+            }
+        }
+        return N;
+    }
+
+
+    //XOR has quite unique prop
+    // XOR(a,a) = 0 & XOR(a,0) = a
+    public static int findMissingNumXOR(int[] arr, int N){
+        int xor1=0, xor2=0;
+        for(int i=0;i<N-1;i++){
+            xor1 = xor1 ^ arr[i];
+            xor2 = xor2^ (i+1);
+        }
+        xor2 = xor2 ^ N;
+        return xor1 ^ xor2;
+    }
+
     public static int[] inputArray(){
         System.out.println("Enter N");
         Scanner sc  = new Scanner(System.in);
@@ -44,5 +71,8 @@ public class MissingNumberInArray {
 
         System.out.println("Missing number by bruteforce approach is : "+findMissingNumBrute(arr, arr.length+1));
         System.out.println("Missing number by using SUM formula is : "+ findMissingNumSum(arr, arr.length+1));
+        System.out.println("Missing number by using Hashing is : "+ findMissingNumHash(arr, arr.length+1));
+        System.out.println("Missing number by using XOR is : "+ findMissingNumXOR(arr, arr.length+1));
+
     }
 }
